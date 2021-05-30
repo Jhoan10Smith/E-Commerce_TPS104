dcf73611de91a68ac5583fad3106a7ec272b2e3d
Jhoan Smith Bonilla <jhoan10smith@gmail.com>
2021-05-29 20:25:40 -0500

    Realizo el CRUD de la tabla empleado

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -15,21 +15,48 @@ class EmpleadoController extends Controller
     	return view('empleado.empleadoListado', compact('empleado'));
     }
 
-
-    public function editar(empleado $empleado)
+    public function edit(empleado $empleado)
     {
-    	return $empleado;
+    	return view('empleado.empleadoEditar', compact('empleado'));
+    }
+
+    public function update(request $editInfo, empleado $empleado)
+    {
+        $empleado->nombreEmpleado=$editInfo->nombre;
+        $empleado->apellidoEmpleado=$editInfo->apellido;  
+        $empleado->usuario=$editInfo->usuario;
+        
+        $empleado->save();
+        $empleado=empleado::all();
+        return redirect()->route('empleado.index',compact('empleado'));
     }
 
     public function delete(empleado $empleado)
     {
-    	return $empleado;
-
-    	// $empleado = empleado::all();
-
-    	// return view('empleado.empleadoListado', compact('empleado'));
-
+    	// return $empleado;
+        $empleado->delete();
+    	$empleado=empleado::all();
+        return redirect()->route('empleado.index', compact('empleado'));
     }
+
+    public function new()
+    {
+        return view('empleado.empleadoNuevo');
+    }
+    public function save(request $newInfo)
+    {
+        $empleado = new empleado();
+
+        $empleado->nombreEmpleado=$newInfo->nombre;
+        $empleado->apellidoEmpleado=$newInfo->apellido;
+        $empleado->usuario=$newInfo->usuario;
+
+        $empleado->save();
+        $empleado=empleado::all();
+
+        return redirect()->route('empleado.index',compact('empleado'));
+    }
+
 
 
 }
